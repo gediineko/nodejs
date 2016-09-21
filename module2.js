247738eb3cc2e3cd86ae6a1635e1da68498ae6df
Geraldine Golong <gediineko14@gmail.com>
2016-09-21 10:38:59 +0800

    used http to host web server

--- module2.js
+++ module2.js
@@ -1,2 +1,9 @@
-var m2 = require('./module1.js');
-console.log(m2);
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log('got a request!');
+    response.write('hi');
+    response.end();
+})
+
+server.listen(3000);
